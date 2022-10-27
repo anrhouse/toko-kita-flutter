e9e71e781754d935507b47eb1948420703bb2a86
anrhouse <aminnurraiskleca@gmail.com>
2022-10-27 09:17:48 +0700

    edit lib\ui

--- lib/ui/produk_page.dart
+++ lib/ui/produk_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:tokokita/bloc/logout_bloc.dart';
+import 'package:tokokita/bloc/produk_bloc.dart';
 import 'package:tokokita/model/produk.dart';
+import 'package:tokokita/ui/login_page.dart';
 import 'package:tokokita/ui/produk_form.dart';
 import 'package:tokokita/ui/produk_detail.dart';
 
@@ -38,22 +41,45 @@ class _ProdukPageState extends State<ProdukPage> {
             ListTile(
               title: const Text("Logout"),
               trailing: const Icon(Icons.logout),
-              onTap: () async {},
+              onTap: () async {
+                await LogoutBloc.logout().then((value) => {
+                      Navigator.pushReplacement(context,
+                          MaterialPageRoute(builder: (context) => LoginPage()))
+                    });
+              },
             )
           ],
         ),
       ),
-      body: ListView(
-        children: [
-          ItemProduk(
-            produk: Produk(
-                id: 1,
-                kodeProduk: "A001",
-                namaProduk: "Kamera",
-                hargaProduk: 5000000),
-          ),
-        ],
+      body: FutureBuilder<List>(
+        future: ProdukBloc.getProduks(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) print(snapshot.error);
+          return snapshot.hasData
+              ? ListProduk(list: snapshot.data)
+              : const Center(
+                  child: CircularProgressIndicator(),
+                );
+        },
       ),
+    );
+  }
+}
+
+class ListProduk extends StatelessWidget {
+  final List? list;
+
+  const ListProduk({Key? key, this.list}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: list == null ? 0 : list!.length,
+      itemBuilder: (context, i) {
+        return ItemProduk(
+          produk: list![i],
+        );
+      },
     );
   }
 }
